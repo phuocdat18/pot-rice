2f5feac7db8cc43a01ea2fd26e3d0563328b4915
baothitran <baothitran.ntp@gmail.com>
2023-10-03 14:36:37 +0700

    aaa

--- src/main/java/com/cg/user/dto/UserCreationParam.java
+++ src/main/java/com/cg/user/dto/UserCreationParam.java
@@ -3,35 +3,33 @@ package com.cg.user.dto;
 import com.cg.role.dto.RoleResult;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Getter
 @Setter
 public class UserCreationParam {
 
-    @NotBlank(message = "The Full Name is required")
-    @Size(min = 5, max = 50, message = "The length of Full Name must be between 5 and 50 characters")
+    @NotNull(message = "{validate.user.fullName.notBlank}")
+    @Length(min = 5, max = 50, message = "The length of Full Name must be between 5 and 50 characters")
     private String fullName;
 
-    @NotBlank(message = "The username is required")
-    @Size(min = 5, max = 30, message = "The length of username must be between 5 and 20 characters")
+    @NotNull(message = "{validate.user.username.notBlank}")
+    @Length(min = 5, max = 30, message = "The length of username must be between 5 and 20 characters")
     private String username;
 
-    @NotBlank(message = "The password is required")
-    @Size(min = 6, max = 30, message = "The length of password must be between 6 and 20 characters")
+    @NotNull(message = "{validate.user.password.notBlank}")
+    @Length(min = 6, max = 30, message = "The length of password must be between 6 and 20 characters")
     private String password;
 
-    @NotBlank(message = "The email is required")
+    @NotNull(message = "{validate.user.email.notBlank}")
     @Email(message = "The email address is invalid")
-    @Size(max = 100, message = "The length of email maximum 100 characters")
+    @Length(max = 100, message = "The length of email maximum 100 characters")
     private String email;
 
-    @NotBlank(message = "The phone is required")
+    @NotNull(message = "{validate.user.phone.notBlank}")
     @Pattern(regexp = "^(\\\\+?84|0)(3[2-9]|5[2689]|7[06-9]|8[1-9]|9[0-9])[0-9]{7}$", message = "Invalid phone number")
     private String phone;
 
